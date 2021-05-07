79f30001a5afffb326a3306396e24fc1b2c2996b
Jason Goldberger <jasongoldberger@gmail.com>
2021-05-07 13:41:27 -0700

    move cast/dump/load in EctoType to before_compile

--- lib/ecto_type.ex
+++ lib/ecto_type.ex
@@ -5,30 +5,36 @@ defmodule Deftype.EctoType do
     quote do
       use Ecto.Type
 
-      def type, do: :map
-
-      def cast(data) when is_map(data) do
-        data
-        |> changeset()
-        |> Deftype.EctoUtil.changeset_to_result()
-      end
-
-      def dump(%__MODULE__{} = data) when is_map(data) do
-        {:ok, Map.drop(data, [:__struct__, :__meta__])}
-      end
-
-      def load(data) when is_map(data) do
-        data
-        |> changeset()
-        |> Deftype.EctoUtil.changeset_to_result()
-      end
-
       @before_compile Deftype.EctoType
     end
   end
 
   defmacro __before_compile__(_env) do
     quote do
+      def type, do: :map
+
+      if !Module.defines?(__MODULE__, {:load, 1}) do
+        def load(data) when is_map(data) do
+          data
+          |> changeset()
+          |> Deftype.EctoUtil.changeset_to_result()
+        end
+      end
+
+      if !Module.defines?(__MODULE__, {:dump, 1}) do
+        def dump(%__MODULE__{} = data) when is_map(data) do
+          {:ok, Map.drop(data, [:__struct__, :__meta__])}
+        end
+      end
+
+      if !Module.defines?(__MODULE__, {:cast, 1}) do
+        def cast(data) when is_map(data) do
+          data
+          |> changeset()
+          |> Deftype.EctoUtil.changeset_to_result()
+        end
+      end
+
       def cast(_), do: :error
 
       def load(_), do: :error
